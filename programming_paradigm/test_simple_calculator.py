8398d553b838ea6734679ea74086a53e9798b89d
fenet128t <fenet128t@gmail.com>
2025-11-29 23:52:44 +0300

    bank account updated

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,5 +1,3 @@
-# test_simple_calculator.py
-
 import unittest
 from simple_calculator import SimpleCalculator
 
@@ -10,23 +8,23 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(1, 2), 3)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(5, 10), -5)
-        self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):           # EXACTLY this name – not "test_multiplication"
+    def test_multiply(self):           # THIS NAME IS REQUIRED
         self.assertEqual(self.calc.multiply(3, 7), 21)
         self.assertEqual(self.calc.multiply(5, 0), 0)
-        self.assertEqual(self.calc.multiply(-4, 5), -20)
+        self.assertEqual(self.calc.multiply(-4, 6), -24)
 
-    def test_divide(self):             # EXACTLY this name – not "test_division"
+    def test_divide(self):             
         self.assertEqual(self.calc.divide(10, 2), 5.0)
-        self.assertEqual(self.calc.divide(8, 4), 2.0)
+        self.assertEqual(self.calc.divide(9, 3), 3.0)
         self.assertIsNone(self.calc.divide(5, 0))
 
 
