8aa853c855fbdac148402b9e4f83b1d694b288ba
fenet128t <fenet128t@gmail.com>
2025-12-11 23:53:04 +0300

    book class updated

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,16 +1,14 @@
 class Book:
-    
-    def __init__(self, title: str, author: str, year: int) -> None:
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
     
-    def __del__(self) -> None:
-        
-        print(f"Deleting {self.title}")
+    def __del__(self):
+        print("Deleting " + self.title)
     
-    def __str__(self) -> str:
-        return f"{self.title} by {self.author}, published in {self.year}"
+    def __str__(self):
+        return self.title + " by " + self.author + ", published in " + str(self.year)
     
-    def __repr__(self) -> str:
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+    def __repr__(self):
+        return "Book('" + self.title + "', '" + self.author + "', " + str(self.year) + ")"
